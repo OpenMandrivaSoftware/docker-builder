3f7380c09111f3419d9e5e468558467002f38023
Alexander Khryukin <alexander@mezon.ru>
2015-10-09 00:36:47 +0300

    drop weird cleanup and no remove chroot after all

--- build-rpm.sh
+++ build-rpm.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -x
+
 MOCK_BIN=/usr/bin/mock-urpm
 MOCK_CONF_FOLDER=/etc/mock-urpm
 MOUNT_POINT=/rpmbuild
@@ -14,21 +16,6 @@ if [ -z "$SOURCE_RPM" ] && [ -z "$SPEC_FILE" ]; then
         exit 1
 fi
 
-#If proxy env variable is set, add the proxy value to the configuration file
-if [ ! -z "$HTTP_PROXY" ] || [ ! -z "$http_proxy" ]; then
-        TEMP_PROXY=""
-        if [ ! -z "$HTTP_PROXY" ]; then
-                TEMP_PROXY=$(echo $HTTP_PROXY | sed s/\\//\\\\\\//g)
-        fi
-        if [ ! -z "$http_proxy" ]; then
-                TEMP_PROXY=$(echo $http_proxy | sed s/\\//\\\\\\//g)
-        fi
-
-        echo "Configuring http proxy to the mock build file to: $TEMP_PROXY"
-        cp $MOCK_CONF_FOLDER/$MOCK_CONFIG.cfg /tmp/$MOCK_CONFIG.cfg
-        sed s/\\[main\\]/\[main\]\\\nproxy=$TEMP_PROXY/g /tmp/$MOCK_CONFIG.cfg > $MOCK_CONF_FOLDER/$MOCK_CONFIG.cfg
-fi
-
 if [ ! -d "$OUTPUT_FOLDER" ]; then
         mkdir -p $OUTPUT_FOLDER
 else
@@ -38,11 +25,13 @@ fi
 echo "=> Building parameters:"
 echo "========================================================================"
 echo "      MOCK_CONFIG:    $MOCK_CONFIG"
+
 #Priority to SOURCE_RPM if both source and spec file env variable are set
+
 if [ ! -z "$SOURCE_RPM" ]; then
         echo "      SOURCE_RPM:     $SOURCE_RPM"
         echo "========================================================================"
-        $MOCK_BIN -v --configdir=$MOCK_CONF_FOLDER -r $MOCK_CONFIG --rebuild $MOUNT_POINT/$SOURCE_RPM --resultdir=$OUTPUT_FOLDER
+        $MOCK_BIN -v --configdir=$MOCK_CONF_FOLDER -r $MOCK_CONFIG --rebuild $MOUNT_POINT/$SOURCE_RPM --no-cleanup-after --resultdir=$OUTPUT_FOLDER
 elif [ ! -z "$SPEC_FILE" ]; then
         if [ -z "$SOURCES" ]; then
                 echo "You need to specify SOURCES env variable pointing to folder or sources file (only when building with SPEC_FILE)"
@@ -51,8 +40,8 @@ elif [ ! -z "$SPEC_FILE" ]; then
         echo "      SPEC_FILE:     $SPEC_FILE"
         echo "      SOURCES:       $SOURCES"
         echo "========================================================================"
-        $MOCK_BIN -v --configdir=$MOCK_CONF_FOLDER -r $MOCK_CONFIG --buildsrpm --spec=$MOUNT_POINT/$SPEC_FILE --sources=$MOUNT_POINT/$SOURCES --resultdir=$OUTPUT_FOLDER
-        $MOCK_BIN -v --configdir=$MOCK_CONF_FOLDER -r $MOCK_CONFIG --rebuild $(find $OUTPUT_FOLDER -type f -name "*.src.rpm") --resultdir=$OUTPUT_FOLDER
+        $MOCK_BIN -v --configdir=$MOCK_CONF_FOLDER -r $MOCK_CONFIG --buildsrpm --spec=$MOUNT_POINT/$SPEC_FILE --sources=$MOUNT_POINT/$SOURCES --no-cleanup-after --resultdir=$OUTPUT_FOLDER
+        $MOCK_BIN -v --configdir=$MOCK_CONF_FOLDER -r $MOCK_CONFIG --rebuild $(find $OUTPUT_FOLDER -type f -name "*.src.rpm") --no-cleanup-after --no-clean --resultdir=$OUTPUT_FOLDER
 fi
 
 echo "Build finished. Check results inside the mounted volume folder."
