feb9e51d7dd4a62e72878efca2ebdc9acc5cd2da
Alexander Khryukin <alexander@mezon.ru>
2015-10-12 12:26:45 +0300

    cleanup

--- build-rpm.sh
+++ build-rpm.sh
@@ -2,19 +2,9 @@
 set -x
 
 MOCK_BIN=/usr/bin/mock-urpm
-MOCK_CONF_FOLDER=/etc/mock-urpm
-MOUNT_POINT=$HOME/$PACKAGE
+config_dir=/etc/mock-urpm/
+build_package=$HOME/$PACKAGE
 OUTPUT_FOLDER=${HOME}/output
-
-if [ -z "$MOCK_CONFIG" ]; then
-        echo "MOCK_CONFIG is empty. Should bin one of: "
-        ls -l $MOCK_CONF_FOLDER
-fi
-if [ -z "$SOURCE_RPM" ] && [ -z "$SPEC_FILE" ]; then
-        echo "You need to provide the src.rpm or spec file to build"
-        echo "Set SOURCE_RPM or SPEC_FILE environment variables"
-        exit 1
-fi
 
 if [ ! -d "$OUTPUT_FOLDER" ]; then
         mkdir -p $OUTPUT_FOLDER
@@ -22,30 +12,28 @@ else
         rm -f $OUTPUT_FOLDER/*
 fi
 
-echo "=> Building parameters:"
-echo "========================================================================"
-echo "      MOCK_CONFIG:    $MOCK_CONFIG"
+generate_config() {
+# Change output format for mock-urpm
+sed '17c/format: %(message)s' $config_dir/logging.ini > ~/logging.ini
+mv -f ~/logging.ini $config_dir/logging.ini
+}
 
-#Priority to SOURCE_RPM if both source and spec file env variable are set
+build_rpm() {
 
-build_me() {
-if [ ! -z "$SOURCE_RPM" ]; then
-        echo "      SOURCE_RPM:     $SOURCE_RPM"
-        echo "========================================================================"
-        $MOCK_BIN -v --configdir=$MOCK_CONF_FOLDER -r $MOCK_CONFIG --rebuild $MOUNT_POINT/$SOURCE_RPM --no-cleanup-after --resultdir=$OUTPUT_FOLDER
-elif [ ! -z "$SPEC_FILE" ]; then
-        if [ -z "$SOURCES" ]; then
-                echo "You need to specify SOURCES env variable pointing to folder or sources file (only when building with SPEC_FILE)"
-                exit 1;
-        fi
-        echo "      SPEC_FILE:     $SPEC_FILE"
-        echo "      SOURCES:       $SOURCES"
-        echo "========================================================================"
-        $MOCK_BIN -v --configdir=$MOCK_CONF_FOLDER -r $MOCK_CONFIG --buildsrpm --spec=$MOUNT_POINT/$SPEC_FILE --sources=$MOUNT_POINT/$SOURCES --no-cleanup-after --resultdir=$OUTPUT_FOLDER
-        $MOCK_BIN -v --configdir=$MOCK_CONF_FOLDER -r $MOCK_CONFIG --rebuild $(find $OUTPUT_FOLDER -type f -name "*.src.rpm") --no-cleanup-after --no-clean --resultdir=$OUTPUT_FOLDER
+echo '--> Build src.rpm'
+$MOCK_BIN -v --configdir=$config_dir --buildsrpm --spec=$build_package/${PACKAGE}.spec --sources=$build_package --no-cleanup-after --resultdir=$OUTPUT_FOLDER
+# Save exit code
+rc=$?
+kill $subshellpid
+echo '--> Done.'
+
+# Check exit code after build
+if [ $rc != 0 ] ; then
+  echo '--> Build failed: mock-urpm encountered a problem.'
+  exit 1
 fi
 
-echo "Build finished. Check results inside the mounted volume folder."
+$MOCK_BIN -v --configdir=$config_dir --rebuild $OUTPUT_FOLDER/${PACKAGE}-*.src.rpm --no-cleanup-after --no-clean --resultdir=$OUTPUT_FOLDER
 }
 
 clone_repo() {
@@ -60,10 +48,8 @@ pushd $HOME/${PACKAGE}
 abf_yml -p .
 popd
 
-ls -la $HOME
-
 # build package
-build_me
 }
 
 clone_repo
+build_rpm
