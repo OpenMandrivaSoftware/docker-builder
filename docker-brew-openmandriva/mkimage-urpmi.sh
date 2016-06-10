433ab93e81c4ff2c5ba0332e9ca04fade732b34e
Tomasz Paweł Gajc <tpgxyz@gmail.com>
2016-06-10 14:04:27 +0200

    clean chroot on errors

--- docker-brew-openmandriva/mkimage-urpmi.sh
+++ docker-brew-openmandriva/mkimage-urpmi.sh
@@ -17,69 +17,79 @@ eval set -- "$optTemp"
 unset optTemp
 
 while true; do
-        case "$1" in
-                -d|--rootfs) rootfsdir=$2 ; shift 2 ;;
-                -v|--version) installversion="$2" ; shift 2 ;;
-                -m|--mirror) mirror="$2" ; shift 2 ;;
-                -a|--arch) arch="$2" ; shift 2 ;;
-                -s|--with-systemd) systemd=true ; shift ;;
-                -h|--help) usage ;;
-                 --) shift ; break ;;
-        esac
+	case "$1" in
+			-d|--rootfs) rootfsdir=$2 ; shift 2 ;;
+			-v|--version) installversion="$2" ; shift 2 ;;
+			-m|--mirror) mirror="$2" ; shift 2 ;;
+			-a|--arch) arch="$2" ; shift 2 ;;
+			-s|--with-systemd) systemd=true ; shift ;;
+			-h|--help) usage ;;
+			--) shift ; break ;;
+	esac
 done
-
 
 target_dir="$rootfsdir/rootfs"
 
+errorCatch() {
+    echo "Something went wrong. Exiting"
+	rm -rf $target_dir
+    exit 1
+}
+
+trap errorCatch ERR SIGHUP SIGINT SIGTERM
+
 if [ -z $installversion ]; then
-        # Attempt to match host version
-        if [ -r /etc/distro-release ]; then
-                installversion="$(rpm --eval %distro_release)"
-        else
-                echo "Error: no version supplied and unable to detect host openmandriva version"
-                exit 1
-        fi
+# Attempt to match host version
+		if [ -r /etc/distro-release ]; then
+			installversion="$(rpm --eval %distro_release)"
+		else
+			echo "Error: no version supplied and unable to detect host openmandriva version"
+			exit 1
+		fi
 fi
 
 if [ -z $mirror ]; then
-        # No repo provided, use main
-        mirror=http://abf-downloads.openmandriva.org/$installversion/repository/$arch/main/release/
+# No repo provided, use main
+	mirror=http://abf-downloads.openmandriva.org/$installversion/repository/$arch/main/release/
 fi
 
 # run me here
 install_chroot(){
 	urpmi.addmedia main_release $mirror --urpmi-root "$target_dir";
 	urpmi basesystem-minimal urpmi distro-release-OpenMandriva locales locales-en $systemd \
-	--auto \
-        --no-suggests \
-        --no-verify-rpm \
-        --urpmi-root "$target_dir" \
-        --root "$target_dir"
+		--auto \
+		--no-suggests \
+		--no-verify-rpm \
+		--urpmi-root "$target_dir" \
+		--root "$target_dir"
+
+	[ $? -gt 0 ] && errorCatch
 }
+
 install_chroot
 
 if [ ! -z $systemd ]; then
-        echo -e "--------------------------------------"
-        echo -e "Creating image with systemd support."
-        echo -e "--------------------------------------\n"
-        systemd="systemd"
+	echo -e "--------------------------------------"
+	echo -e "Creating image with systemd support."
+	echo -e "--------------------------------------\n"
+	systemd="systemd"
 fi
 
 if [ ! -z $systemd ]; then
-        #Prevent systemd from starting unneeded services
-        (cd $target_dir/lib/systemd/system/sysinit.target.wants/; for i in *; do [ $i == systemd-tmpfiles-setup.service ] || rm -f $i; done); \
-        rm -f $target_dir/lib/systemd/system/multi-user.target.wants/*;\
-        rm -f $target_dir/etc/systemd/system/*.wants/*;\
-        rm -f $target_dir/lib/systemd/system/local-fs.target.wants/*; \
-        rm -f $target_dir/lib/systemd/system/sockets.target.wants/*udev*; \
-        rm -f $target_dir/lib/systemd/system/sockets.target.wants/*initctl*; \
-        rm -f $target_dir/lib/systemd/system/basic.target.wants/*;\
-        rm -f $target_dir/lib/systemd/system/anaconda.target.wants/*;
+#Prevent systemd from starting unneeded services
+	(cd $target_dir/lib/systemd/system/sysinit.target.wants/; for i in *; do [ $i == systemd-tmpfiles-setup.service ] || rm -f $i; done); \
+		rm -f $target_dir/lib/systemd/system/multi-user.target.wants/*;\
+		rm -f $target_dir/etc/systemd/system/*.wants/*;\
+		rm -f $target_dir/lib/systemd/system/local-fs.target.wants/*; \
+		rm -f $target_dir/lib/systemd/system/sockets.target.wants/*udev*; \
+		rm -f $target_dir/lib/systemd/system/sockets.target.wants/*initctl*; \
+		rm -f $target_dir/lib/systemd/system/basic.target.wants/*;\
+		rm -f $target_dir/lib/systemd/system/anaconda.target.wants/*;
 fi
 
 if [ -d "$target_dir/etc/sysconfig" ]; then
-        # allow networking init scripts inside the container to work without extra steps
-        echo 'NETWORKING=yes' > "$target_dir/etc/sysconfig/network"
+# allow networking init scripts inside the container to work without extra steps
+	echo 'NETWORKING=yes' > "$target_dir/etc/sysconfig/network"
 fi
 
 # make sure /etc/resolv.conf has something useful in it
@@ -90,9 +100,9 @@ nameserver 8.8.4.4
 EOF
 
 if [ ! -z $systemd ]; then
-    tarFile="$rootfsdir/rootfs-${arch}-systemd.tar.xz"
+	tarFile="$rootfsdir/rootfs-${arch}-systemd.tar.xz"
 else
-    tarFile="$rootfsdir/rootfs-${arch}.tar.xz"
+	tarFile="$rootfsdir/rootfs-${arch}.tar.xz"
 fi
 
 pushd $target_dir
