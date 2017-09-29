dfad2fadfeaa65db1a83a06df4b6faab5674b52b
tpgxyz <tpgxyz@gmail.com>
2017-09-29 19:58:41 +0200

    preserve pathnames

--- cachedchroot.sh
+++ cachedchroot.sh
@@ -128,7 +128,7 @@ for arch in $arches ; do
 
     # xz options -4e is 4th extreme level of compression, and -T0 is to use all available threads to speedup compress
     # need sudo to pack root:root dirs
-    sudo XZ_OPT="-4e -T0" tar -Jcvf --no-acls ${OUTPUT_FOLDER}/${chroot}.tar.xz ${chroot_path}/${chroot}
+    sudo XZ_OPT="-4e -T0" tar -Jcvf --no-xattrs --no-acls --absolute-paths ${OUTPUT_FOLDER}/${chroot}.tar.xz ${chroot_path}/${chroot}
 
     # Save exit code
     rc=$?
