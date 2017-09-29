05deec9b110cb6959a4caed0d251345a6319cff4
tpgxyz <tpgxyz@gmail.com>
2017-09-30 00:36:35 +0200

    use 7e for xz compression

--- cachedchroot.sh
+++ cachedchroot.sh
@@ -126,9 +126,9 @@ for arch in $arches ; do
 	exit 1
     fi
 
-    # xz options -4e is 4th extreme level of compression, and -T0 is to use all available threads to speedup compress
+    # xz options -7e is 7th extreme level of compression, and -T0 is to use all available threads to speedup compress
     # need sudo to pack root:root dirs
-    sudo XZ_OPT="-4e -T0" tar --no-xattrs --no-acls --absolute-paths -Jcvf "${OUTPUT_FOLDER}"/"${chroot}".tar.xz "${chroot_path}"/"${chroot}"
+    sudo XZ_OPT="-7e -T0" tar --no-xattrs --no-acls --absolute-paths -Jcvf "${OUTPUT_FOLDER}"/"${chroot}".tar.xz "${chroot_path}"/"${chroot}"
 
     # Save exit code
     rc=$?
