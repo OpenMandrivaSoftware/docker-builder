af3cd6ed82d037c86aa20c04fb4a07f440bf1275
Neal Gompa <ngompa13@gmail.com>
2018-03-04 17:33:52 -0500

    WIP porting to DNF

--- config-generator.sh
+++ config-generator.sh
@@ -2,7 +2,6 @@
 echo 'OpenMandriva platform config generator'
 
 extra_cfg_options="$EXTRA_CFG_OPTIONS"
-extra_cfg_urpm_options="$EXTRA_CFG_URPM_OPTIONS"
 uname="$UNAME"
 email="$EMAIL"
 platform_arch="$PLATFORM_ARCH"
@@ -10,7 +9,7 @@ platform_name=${PLATFORM_NAME:-"openmandriva"}
 repo_url="$REPO_URL"
 repo_names="$REPO_NAMES"
 
-default_cfg=/etc/mock-urpm/default.cfg
+default_cfg=/etc/mock/default.cfg
 gen_included_repos() {
 
 names_arr=($repo_names)
@@ -18,56 +17,61 @@ urls_arr=($repo_url)
 
 for (( i=0; i<${#names_arr[@]}; i++ ));
 do
-	echo '"'${names_arr[i]}'"': '"'${urls_arr[i]}'"', >> $default_cfg
+	printf "[${names_arr[i]}]\nname=${names_arr[i]}\nbaseurl=${urls_arr[i]}\ngpgcheck=0\nenabled=1\n\n" >> $default_cfg
 done
-# close urpmi repos section
-echo '}' >> $default_cfg
+# close dnf repos section
+echo '"""' >> $default_cfg
 }
 
 if [ "$platform_arch" = 'aarch64' ]; then
 cat <<EOF> $default_cfg
-config_opts['target_arch'] = '$platform_arch --without check --without uclibc --without dietlibc'
+config_opts['target_arch'] = '$platform_arch'
 config_opts['legal_host_arches'] = ('i586', 'i686', 'x86_64', 'aarch64')
-config_opts['urpmi_options'] = '--no-suggests --no-verify-rpm --ignoresize --ignorearch --excludedocs --downloader wget --fastunsafe --nolock $extra_cfg_options'
-config_opts['urpm_options'] = '--xml-info=never $extra_cfg_urpm_options'
 EOF
 
 elif [ "$platform_arch" = 'armv7hl' ]; then
 cat <<EOF> $default_cfg
-config_opts['target_arch'] = '$platform_arch --without check --without uclibc'
+config_opts['target_arch'] = '$platform_arch'
 config_opts['legal_host_arches'] = ('i586', 'i686', 'x86_64', 'armv7hl', 'armv7l', 'aarch64')
-config_opts['urpmi_options'] = '--no-suggests --no-verify-rpm --ignoresize --ignorearch --excludedocs --downloader wget --fastunsafe --nolock $extra_cfg_options'
-config_opts['urpm_options'] = '--xml-info=never $extra_cfg_urpm_options'
 EOF
 else
 
 cat <<EOF> $default_cfg
-config_opts['target_arch'] = '$platform_arch --without uclibc'
+config_opts['target_arch'] = '$platform_arch'
 config_opts['legal_host_arches'] = ('i586', 'i686', 'x86_64')
-config_opts['urpmi_options'] = '--no-suggests --no-verify-rpm --ignoresize --excludedocs --downloader wget --fastunsafe --nolock $extra_cfg_options'
-config_opts['urpm_options'] = '$extra_cfg_urpm_options'
 EOF
 fi
 
 cat <<EOF>> $default_cfg
 config_opts['root'] = '$platform_name-$platform_arch'
-config_opts['chroot_setup'] = 'basesystem-minimal locales locales-en distro-release-OpenMandriva gnupg rpm-build wget task-devel clang'
-#config_opts['urpm_options'] = '--xml-info=never $extra_cfg_urpm_options'
+config_opts['chroot_setup'] = 'basesystem-minimal locales locales-en distro-release-OpenMandriva gnupg rpm-build wget task-devel clang openmandriva-repos-pkgprefs'
+config_opts['package_manager'] = 'dnf'
+config_opts['dnf_common_opts'] = ['--disableplugin=local', '--setopt=deltarpm=False', '--forcearch=$platform_arch']
+config_opts['useradd'] = '/usr/sbin/useradd -o -m -u %(uid)s -g %(gid)s -d %(home)s %(user)s'
+config_opts['releasever'] = '0' # at some point, this should be set by ABF
+config_opts['use_nspawn'] = False
 
-# If it's True - current urpmi configs will be copied to the chroot.
-# Ater that other media will be added.
-# config_opts['use_system_media'] = True
-
-config_opts['plugin_conf']['root_cache_enable'] = False
-config_opts['plugin_conf']['ccache_enable'] = False
-config_opts['use_system_media'] = False
-config_opts['basedir'] = '/var/lib/mock-urpm/'
-config_opts['cache_topdir'] = '/var/cache/mock-urpm/'
+config_opts['basedir'] = '/var/lib/mock/'
+config_opts['cache_topdir'] = '/var/cache/mock/'
 
 config_opts['dist'] = 'cooker'  # only useful for --resultdir variable subst
 config_opts['macros']['%packager'] = '$uname <$email>'
 config_opts['macros'] = { '%_topdir': '%s/build' % config_opts['chroothome'], }
-config_opts["urpmi_media"] = {
+config_opts['yum.conf'] = """
+[main]
+keepcache=1
+debuglevel=2
+reposdir=/dev/null
+retries=20
+obsoletes=1
+gpgcheck=0
+assumeyes=1
+syslog_ident=mock
+syslog_device=
+install_weak_deps=0
+metadata_expire=0
+best=1
+
 EOF
 
 gen_included_repos
