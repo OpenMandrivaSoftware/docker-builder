10f7230f9df96e1a6680a69629ab076bb39f2b16
Tomasz Paweł Gajc <tpgxyz@gmail.com>
2018-03-07 12:11:13 +0100

    adjust config for cached-chroot

--- config-generator.sh
+++ config-generator.sh
@@ -58,8 +58,8 @@ config_opts['cache_topdir'] = '/var/cache/mock/'
 config_opts['dist'] = 'cooker'  # only useful for --resultdir variable subst
 config_opts['macros']['%packager'] = '$uname <$email>'
 config_opts['macros'] = { '%_topdir': '%s/build' % config_opts['chroothome'], }
-config_opts['plugin_conf']['root_cache_opts']['compress_program'] = ""
-config_opts['plugin_conf']['root_cache_opts']['extension'] = ""
+config_opts['plugin_conf']['root_cache_opts']['compress_program'] = "xz"
+config_opts['plugin_conf']['root_cache_opts']['extension'] = ".xz"
 config_opts['yum.conf'] = """
 [main]
 keepcache=1
