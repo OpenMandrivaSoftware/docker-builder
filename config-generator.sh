7011e2e388bf54ed93677159410a0f112352d379
Neal Gompa <ngompa13@gmail.com>
2018-03-10 14:09:19 -0500

    Drop rpm_command setting, we now have a symlink in rpm package

--- config-generator.sh
+++ config-generator.sh
@@ -52,7 +52,6 @@ config_opts['dnf_builddep_opts'] = ['--forcearch=$platform_arch']
 config_opts['useradd'] = '/usr/sbin/useradd -o -m -u %(uid)s -g %(gid)s -d %(home)s %(user)s'
 config_opts['releasever'] = '0' # at some point, this should be set by ABF
 config_opts['use_nspawn'] = False
-config_opts['rpm_command'] = '/usr/bin/rpm'
 
 config_opts['basedir'] = '/var/lib/mock/'
 config_opts['cache_topdir'] = '/var/cache/mock/'
