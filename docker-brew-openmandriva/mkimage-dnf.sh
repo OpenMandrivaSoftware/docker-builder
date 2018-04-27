749503b5355206dd09c4958ba334e39c792250a2
Alexander Khryukin <alexander@mezon.ru>
2018-04-27 18:17:14 +0300

    fix mkimage-dnf

--- docker-brew-openmandriva/mkimage-dnf.sh
+++ docker-brew-openmandriva/mkimage-dnf.sh
@@ -1,60 +1,57 @@
 #!/usr/bin/env bash
+set -x
 #
-# Script to create OpenMandriva official base images for integration with stackbrew
-# library.
-#
-# Needs to be run from OpenMandriva 4.0 or greater or Mageia 6 or greater, as it requires DNF.
-#
-# Tested working versions are for Mageia 6 onwards (inc. cauldron) and OpenMandriva Cooker.
-#
-# Based on mkimage-urpmi.sh from Mageia
-# Original source: https://github.com/juanluisbaptiste/docker-brew-mageia/blob/cdaf30e99f06f6463bb7729b40322c7ddde624c6/mkimage-dnf.sh
-#
-
 set -e
 
 mkimg="$(basename "$0")"
 
 usage() {
-	echo >&2 "usage: $mkimg --rootfs=rootfs_path --version=openmandriva_version [--mirror=url] [--package-manager=dnf] [--with-systemd]"
-	echo >&2 "   ie: $mkimg --rootfs=. --version=4.0 --with-systemd"
-	echo >&2 "       $mkimg --rootfs=. --version=cooker --package-manager=dnf --with-systemd"
-	echo >&2 "       $mkimg --rootfs=/tmp/rootfs --version=4.0 --mirror=http://abf-downloads.openmandriva.org/4.0/repository/x86_64/ --with-systemd"
-	echo >&2 "       $mkimg --rootfs=. --version=4.0 --package-manager=dnf"
-	exit 1
+    echo >&2 "usage: $mkimg --rootfs=rootfs_path --version=openmandriva_version [--mirror=url]"
+    echo >&2 "       $mkimg --rootfs=/tmp/rootfs --version=3.0 --arch=x86_64 --with-updates"
+    echo >&2 "       $mkimg --rootfs=/tmp/rootfs --version=openmandriva2014.0 --arch=x86_64"
+    echo >&2 "       $mkimg --rootfs=. --version=cooker --mirror=http://abf-downloads.openmandriva.org/cooker/repository/x86_64/main/release/"
+    echo >&2 "       $mkimg --rootfs=. --version=cooker"
+    exit 1
 }
 
-optTemp=$(getopt --options '+d,v:,s,p,h' --longoptions 'rootfs:,version:,mirror:,package-manager:,with-systemd, help' --name mkimage-dnf -- "$@")
+optTemp=$(getopt --options '+d,v:,m:,a:,s,u,U,p,h,+x' --longoptions 'rootfs:,version:,mirror:,arch:,with-systemd,with-updates,without-user,with-passwd,help,extra-package:' --name mkimage-dnf -- "$@")
 eval set -- "$optTemp"
 unset optTemp
 
-installversion=
-mirror=
+extra_packages=""
+
 while true; do
     case "$1" in
-	-d|--rootfs) dir=$2 ; shift 2 ;;
+	-d|--rootfs) rootfsdir=$2 ; shift 2 ;;
 	-v|--version) installversion="$2" ; shift 2 ;;
 	-m|--mirror) mirror="$2" ; shift 2 ;;
-	-p|--package-manager) pkgmgr="$2" ; shift 2 ;;
-	-s|--with-systemd) systemd=true ; shift ;;
+	-a|--arch) arch="$2" ; shift 2 ;;
+	-s|--with-systemd) systemd=systemd ; shift ;;
+	-u|--with-updates) updates=true ; shift ;;
+	-p|--with-passwd) passwd=true ; shift ;;
+	-U|--without-user) without_user=true ; shift ;;
 	-h|--help) usage ;;
+	-x|--extra-package) extra_packages="$extra_packages $2" ; shift 2 ;;
 	--) shift ; break ;;
     esac
 done
 
-#dir="$1"
-rootfsDir="$dir/rootfs"
-#shift
+target_dir="${rootfsdir}/rootfs"
 
+errorCatch() {
+    echo "Error catched. Exiting"
+    rm -rf "${target_dir}"
+    exit 1
+}
 
-#[ "$dir" ] || usage
+trap errorCatch ERR SIGHUP SIGINT SIGTERM
 
 if [ -z "${installversion}" ]; then
-        # Attempt to match host version
-    if [ -r /etc/mandriva-release ]; then
-	installversion="$(sed 's/^[^0-9\]*\([0-9.]\+\).*$/\1/' /etc/mandriva-release)"
+# Attempt to match host version
+    if [ -r /etc/distro-release ]; then
+	installversion="$(rpm --eval %distro_release)"
     else
-	printf '%s\n' "Error: no version supplied and unable to detect host openmandriva version"
+	echo "Error: no version supplied and unable to detect host openmandriva version"
 	exit 1
     fi
 fi
@@ -69,87 +66,127 @@ if [ -z "${mirror}" ]; then
         reposetup="--disablerepo=* --enablerepo=openmandriva-x86_64 --enablerepo=updates-x86_64"
 fi
 
-if [ ! -z "${pkgmgr}" ]; then
-        valid_pkg_mgrs="dnf"
-
-        [[ $valid_pkg_mgrs =~ (^|[[:space:]])$pkgmgr($|[[:space:]]) ]] && true || echo "Invalid package manager selected." && exit 1
-
-        echo -e "--------------------------------------"
-        echo -e "Creating image to use $pkgmgr."
-        echo -e "--------------------------------------\n"
-
-fi
-
 # Must be after the non-empty check or otherwise this will fail
 if [ -z "${pkgmgr}" ]; then
         pkgmgr="dnf"
 fi
 
+
+# run me here
+install_chroot(){
+    dnf \
+    ${reposetup} \
+    --installroot="${target_dir}" \
+    --releasever="${installversion}" \
+    --setopt=install_weak_deps=False \
+    --nodocs --assumeyes \
+    install basesystem-minimal openmandriva-repos ${pkgmgr} locales locales-en ${systemd}
+
+    if [ $? != 0 ]; then
+	echo "Creating dnf chroot failed."
+	errorCatch
+    fi
+}
+
+arm_platform_detector(){
+
+probe_cpu() {
+cpu="$(uname -m)"
+case "${cpu}" in
+   i386|i486|i586|i686|i86pc|BePC|x86_64)
+      cpu="i386"
+   ;;
+   armv[4-9]*)
+      cpu="arm"
+   ;;
+   aarch64)
+      cpu="aarch64"
+   ;;
+esac
+
+# create path
+if [ "${arch}" = 'aarch64' ]; then
+    if [ "${cpu}" != 'aarch64' ]; then
+	mkdir -p "${target_dir}"/usr/bin/
+	sudo sh -c "echo '${arch}-mandriva-linux-gnueabi' > /etc/rpm/platform"
+	cp /usr/bin/qemu-static-aarch64 "${target_dir}"/usr/bin/
+    fi
+fi
+
+if [ "${arch}" = 'armv7hl' ]; then
+    if [ "${cpu}" != 'arm' ] ; then
+	mkdir -p "${target_dir}"/usr/bin/
+	sudo sh -c "echo '${arch}-mandriva-linux-gnueabi' > /etc/rpm/platform"
+	cp /usr/bin/qemu-static-arm "${target_dir}"/usr/bin/
+    fi
+fi
+}
+probe_cpu
+}
+
+arm_platform_detector
+install_chroot
+
 if [ ! -z "${systemd}" ]; then
-        echo -e "--------------------------------------"
-        echo -e "Creating image with systemd support."
-        echo -e "--------------------------------------\n"
-        systemd="systemd" 
-fi
-
-(
-        dnf \
-            ${reposetup} \
-            --installroot="${rootfsDir}" \
-            --releasever="${installversion}" \
-            --setopt=install_weak_deps=False \
-            --nodocs --assumeyes \
-            install basesystem-minimal openmandriva-repos "${pkgmgr}" locales locales-en "${systemd}"
-)
-
-"$(dirname "$BASH_SOURCE")/.febootstrap-minimize" "$rootfsDir"
-
-if [ -d "$rootfsDir/etc/sysconfig" ]; then
-        # allow networking init scripts inside the container to work without extra steps
-        printf '%s\n' 'NETWORKING=yes' > "$rootfsDir/etc/sysconfig/network"
+    printf '%b\n' '--------------------------------------'
+    printf '%b\n' 'Creating image with systemd support.'
+    printf '%b\n' '--------------------------------------'
+    systemd="systemd"
 fi
 
 if [ ! -z "${systemd}" ]; then
-    #Prevent systemd from starting unneeded services
-    (cd "${rootfsDir}"/lib/systemd/system/sysinit.target.wants/; for i in *; do [ "$i" = 'systemd-tmpfiles-setup.service' ] || rm -f "$i"; done); \
-	rm -f "${rootfsDir}"/lib/systemd/system/multi-user.target.wants/*;\
-	rm -f "${rootfsDir}"/etc/systemd/system/*.wants/*;\
-	rm -f "${rootfsDir}"/lib/systemd/system/local-fs.target.wants/*; \
-	rm -f "${rootfsDir}"/lib/systemd/system/sockets.target.wants/*udev*; \
-	rm -f "${rootfsDir}"/lib/systemd/system/sockets.target.wants/*initctl*; \
-	rm -f "${rootfsDir}"/lib/systemd/system/basic.target.wants/*;\
-	rm -f "${rootfsDir}"/lib/systemd/system/anaconda.target.wants/*;
+# Prevent systemd from starting unneeded services
+    (cd "${target_dir}"/lib/systemd/system/sysinit.target.wants/; for i in *; do [ "$i" = 'systemd-tmpfiles-setup.service' ] || rm -f "${i}"; done); \
+	rm -f "${target_dir}"/lib/systemd/system/multi-user.target.wants/*;\
+	rm -f "${target_dir}"/etc/systemd/system/*.wants/*;\
+	rm -f "${target_dir}"/lib/systemd/system/local-fs.target.wants/*; \
+	rm -f "${target_dir}"/lib/systemd/system/sockets.target.wants/*udev*; \
+	rm -f "${target_dir}"/lib/systemd/system/sockets.target.wants/*initctl*; \
+	rm -f "${target_dir}"/lib/systemd/system/basic.target.wants/*;\
+	rm -f "${target_dir}"/lib/systemd/system/anaconda.target.wants/*;
 fi
 
-
-# Docker mounts tmpfs at /dev and procfs at /proc so we can remove them
-rm -rf "$rootfsDir/dev" "$rootfsDir/proc"
-mkdir -p "$rootfsDir/dev" "$rootfsDir/proc"
+if [ -d "${target_dir}"/etc/sysconfig ]; then
+# allow networking init scripts inside the container to work without extra steps
+    echo 'NETWORKING=yes' > "${target_dir}"/etc/sysconfig/network
+fi
 
 # make sure /etc/resolv.conf has something useful in it
-mkdir -p "$rootfsDir/etc"
-cat > "$rootfsDir/etc/resolv.conf" <<'EOF'
+mkdir -p "${target_dir}"/etc
+cat > "${target_dir}"/etc/resolv.conf <<'EOF'
 nameserver 8.8.8.8
 nameserver 8.8.4.4
 EOF
 
-rootfsSuffix=
+if [ ! -z "${without_user}" ]; then
+	# Create user omv, password omv
+	echo 'omv:x:1001:1001::/home/omv:/bin/bash' >>"${target_dir}"/etc/passwd
+	echo 'omv:$6$rG3bQ92hkTNubV1p$5qPB9FoXBhNcSE1FOklCoEDowveAgjSf2cHYVwCENZaWtgpFQaRRRN5Ihwd8nuaKMdA1R1XouOasJ7u5dbiGt0:17302:0:99999:7:::' >> "${target_dir}"/etc/shadow
+	echo 'omv:x:1001:' >>"${target_dir}"/etc/group
+	sed -i -e 's,wheel:x:10:$,wheel:x:10:omv,' "${target_dir}"/etc/group
+fi
 
-if [ ! -z "${pkgmgr}" ]; then
-    rootfsSuffix="-$pkgmgr"
+if [ ! -z "${passwd}" ]; then
+	ROOT_PASSWD="root"
+	echo "change password to ${ROOT_PASSWD}"
+	sudo chroot "${target_dir}" /bin/bash -c "echo '${ROOT_PASSWD}' |passwd root --stdin"
+
+	cat << EOF > "${target_dir}"/README.omv
+OpenMandriva $installversion distro
+default login and password is root:root
+You must change it!
+EOF
 fi
 
 if [ ! -z "${systemd}" ]; then
-    rootfsSuffix="$rootfsSuffix-systemd"
+    tarFile="${rootfsdir}"/rootfs-"${arch}"-systemd.tar.xz
+else
+    tarFile="${rootfsdir}"/rootfs-"${arch}".tar.xz
 fi
 
-tarFile="$dir/rootfs$rootfsSuffix.tar.xz"
-
-touch "$tarFile"
-
-(
-        set -x
-        tar --numeric-owner -caf "$tarFile" -C "$rootfsDir" --transform='s,^./,,' .
-)
-
-( set -x; rm -rf "$rootfsDir" )
+cd "${target_dir}"
+rm -fv usr/bin/qemu-*
+tar --numeric-owner -caf "${tarFile}" -c .
+cd ..
+rm -rf "${target_dir}"
+rm -fv /etc/rpm/platform
