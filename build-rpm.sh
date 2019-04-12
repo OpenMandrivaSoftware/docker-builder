2bbe1b722f92a5347244fcd3086e570dfa3f65bf
Alexander Stefanov <alexander@mezon.ru>
2019-04-13 02:29:17 +0300

    exit_code 1

--- build-rpm.sh
+++ build-rpm.sh
@@ -259,7 +259,7 @@ test_rpm() {
 					printf '%s\n' "Package $i is newer than what's in the repo. Extra tests passed: $test_code" >> "${test_log}"
 				else
 					# Proposed rpm is either the same, older, or another problem
-					test_code='5'
+					test_code='1'
 					printf '%s\n' "Package $i is either the same, older, or another problem. Extra tests failed: $test_code" >> "${test_log}"
 					printf 'Compared %s %s (new) to %s (repo) for %s\n' "$RPM_NAME" "$RPM_EVR" "$REPO_EVR" "$i" >> "${test_log}"
 					rpmdev-vercmp "${RPM_EVR}" "${REPO_EVR}" >> "${test_log}"
