1b0ddcf6463b00725969f1a65e614c02e182d400
Alexander Stefanov <alexander@mezon.ru>
2019-08-15 02:41:18 +0300

    add cooker2rolling.py

---
+++ cooker2rolling.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+import re
+import requests
+import time
+
+
+def request_builds():
+    #    https://abf.openmandriva.org/api/v1/build_lists?filter[build_for_platform_id]=28&filter[status]=6000&filter[ownership]=everything&filter[updated_at_start]=1565540721&per_page=100&page=1
+    # updated_at_start = 1565540721
+    # current time minus 1 days
+    build_ids = []
+    threedaysago = int(time.time()) - 24 * 3600
+    params = {'cooker': 28, 'status': 6000, 'time': threedaysago}
+
+    abf_api = 'https://abf.openmandriva.org'
+    api_link = '{}/api/v1/build_lists?filter[build_for_platform_id]={}&filter[status]={}&filter[ownership]=everything&filter[updated_at_start]={}&per_page=100&page=55'.format(
+        abf_api, params['cooker'], params['status'], params['time'])
+    resp = requests.get(api_link)
+    if resp.status_code == 404:
+        print('requested api [{}] not found'.format(api_link))
+    if resp.status_code == 200:
+        page = resp.content.decode('utf-8')
+        page2 = json.loads(page)
+        name = page2['build_lists']
+        for build_id in name:
+            print(build_id)
+            if build_id['url']:
+                build_ids.append('{}'.format(abf_api) + build_id['url'])
+            else:
+                print('no build ids')
+
+    for build_id in build_ids:
+        print(build_id)
+
+
+request_builds()
