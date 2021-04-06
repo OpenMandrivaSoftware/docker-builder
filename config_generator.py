11908e34d951845f01a23ae7d7e521e9f754c14f
Alexander Stefanov <alexander@mezon.ru>
2021-04-06 16:48:11 +0300

    fix config_generator

--- config_generator.py
+++ config_generator.py
@@ -73,7 +73,7 @@ def generate_config():
 
     print_conf("config_opts['root'] = '%s-%s'" %
                (platform_name, platform_arch))
-    print_conf("config_opts['chroot_setup_cmd'] = ('install', 'basesystem-build', 'dwz', 'dnf')")
+    print_conf("config_opts['chroot_setup_cmd'] = ('install', '@buildsys-build')")
     print_conf("config_opts['package_manager'] = 'dnf'")
     print_conf(
         "config_opts['dnf_common_opts'] = ['--refresh', '--disableplugin=local', '--setopt=deltarpm=False', '--forcearch=%s']" % platform_arch)
