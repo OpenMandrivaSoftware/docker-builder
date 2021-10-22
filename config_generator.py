bb16fbae0fbdf6a819bbc98000af25f8cb47ae33
Alexander Stefanov <alexander@mezon.ru>
2021-10-22 18:19:44 +0300

    disable networking

--- config_generator.py
+++ config_generator.py
@@ -82,7 +82,7 @@ def generate_config():
     print_conf("config_opts['useradd'] = '/usr/sbin/useradd -o -m -u {{chrootuid}} -g {{chrootgid}} -d {{chroothome}} {{chrootuser}}'")
     print_conf("config_opts['releasever'] = '0'")
     print_conf("config_opts['use_host_resolv'] = True")
-    print_conf("config_opts['rpmbuild_networking'] = True")
+    print_conf("config_opts['rpmbuild_networking'] = False")
     print_conf("config_opts['rpmbuild_timeout'] = 86400")
     print_conf("config_opts['isolation'] = 'simple'")
     print_conf("config_opts['use_nspawn'] = False")
