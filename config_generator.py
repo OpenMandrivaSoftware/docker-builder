f8805a49723158f9e66833b8208386e6c0320a9a
Alexander Stefanov <alexander@mezon.ru>
2021-12-01 17:30:55 +0300

    bind mount /etc/resolv.conf

--- config_generator.py
+++ config_generator.py
@@ -51,38 +51,31 @@ def generate_config():
 
     if platform_arch == 'aarch64':
         print_conf("config_opts['target_arch'] = '%s'" % platform_arch)
-        print_conf(
-            "config_opts['legal_host_arches'] = ('i586', 'i686', 'x86_64', 'aarch64')")
+        print_conf("config_opts['legal_host_arches'] = ('i586', 'i686', 'x86_64', 'aarch64')")
     if platform_arch == "armv7hnl":
         print_conf("config_opts['target_arch'] = '%s'" % platform_arch)
-        print_conf(
-            "config_opts['legal_host_arches'] = ('i586', 'i686', 'x86_64', 'armv8hcnl', 'armv8hnl', 'armv8hl', 'armv7hnl', 'armv7hl', 'armv7l', 'aarch64')")
+        print_conf("config_opts['legal_host_arches'] = ('i586', 'i686', 'x86_64', 'armv8hcnl', 'armv8hnl', 'armv8hl', 'armv7hnl', 'armv7hl', 'armv7l', 'aarch64')")
     if platform_arch == "riscv64":
-        print_conf(
-            "config_opts['target_arch'] = '%s --without check --without pgo'" % platform_arch)
-        print_conf(
-            "config_opts['legal_host_arches'] = ('i586', 'i686', 'x86_64', 'riscv64')")
+        print_conf("config_opts['target_arch'] = '%s --without check --without pgo'" % platform_arch)
+        print_conf("config_opts['legal_host_arches'] = ('i586', 'i686', 'x86_64', 'riscv64')")
     if platform_arch == "znver1":
         print_conf("config_opts['target_arch'] = '%s'" % platform_arch)
         print_conf("config_opts['legal_host_arches'] = ('x86_64', 'znver1')")
     accepted_arches = {'x86_64', 'i686', 'i586'}
     if platform_arch in accepted_arches:
         print_conf("config_opts['target_arch'] = '%s'" % platform_arch)
-        print_conf(
-            "config_opts['legal_host_arches'] = ('i586', 'i686', 'x86_64')")
+        print_conf("config_opts['legal_host_arches'] = ('i586', 'i686', 'x86_64')")
 
-    print_conf("config_opts['root'] = '%s-%s'" %
-               (platform_name, platform_arch))
+    print_conf("config_opts['root'] = '%s-%s'" % (platform_name, platform_arch))
     print_conf("config_opts['chroot_setup_cmd'] = ('install', 'basesystem-build', 'dwz', 'dnf')")
     print_conf("config_opts['package_manager'] = 'dnf'")
-    print_conf(
-        "config_opts['dnf_common_opts'] = ['--refresh', '--disableplugin=local', '--setopt=deltarpm=False', '--setopt=install_weak_deps=False', '--setopt=tsflags=nodocs', '--forcearch=%s']" % platform_arch)
-    print_conf(
-        "config_opts['dnf_builddep_opts'] = ['--refresh', '--forcearch=%s']" % platform_arch)
+    print_conf("config_opts['dnf_common_opts'] = ['--refresh', '--disableplugin=local', '--setopt=deltarpm=False', '--setopt=install_weak_deps=False', '--setopt=tsflags=nodocs', '--forcearch=%s']" % platform_arch)
+    print_conf("config_opts['dnf_builddep_opts'] = ['--refresh', '--forcearch=%s']" % platform_arch)
     print_conf("config_opts['useradd'] = '/usr/sbin/useradd -o -m -u {{chrootuid}} -g {{chrootgid}} -d {{chroothome}} {{chrootuser}}'")
     print_conf("config_opts['releasever'] = '0'")
-    print_conf("config_opts['use_host_resolv'] = True")
     print_conf("config_opts['rpmbuild_networking'] = False")
+    print_conf("config_opts['plugin_conf']['bind_mount_enable'] = True")
+    print_conf("config_opts['plugin_conf']['bind_mount_opts']['dirs'].append(('/etc/resolv.conf', '/etc/resolv.conf'))")
     print_conf("config_opts['rpmbuild_timeout'] = 86400")
     print_conf("config_opts['isolation'] = 'simple'")
     print_conf("config_opts['use_nspawn'] = False")
@@ -113,36 +106,25 @@ def generate_config():
         print_conf("config_opts['plugin_conf']['tmpfs_opts']['keep_mounted'] = False")
 
     print_conf("config_opts['dist'] = '%s'" % platform_name)
-    print_conf(
-        "config_opts['macros']['%%packager'] = '%s <%s>'" % (uname, email))
-    print_conf(
-        "config_opts['macros']['%_topdir'] = '%s/build' % config_opts['chroothome']")
-    print_conf(
-        "config_opts['macros']['%_rpmfilename'] = '%%{NAME}-%%{VERSION}-%%{RELEASE}-%%{DISTTAG}.%%{ARCH}.rpm'")
-    print_conf(
-        "config_opts['macros']['%cross_compiling'] = '0' # ABF should generally be considered native builds")
+    print_conf("config_opts['macros']['%%packager'] = '%s <%s>'" % (uname, email))
+    print_conf("config_opts['macros']['%_topdir'] = '%s/build' % config_opts['chroothome']")
+    print_conf("config_opts['macros']['%_rpmfilename'] = '%%{NAME}-%%{VERSION}-%%{RELEASE}-%%{DISTTAG}.%%{ARCH}.rpm'")
+    print_conf("config_opts['macros']['%cross_compiling'] = '0' # ABF should generally be considered native builds")
     print_conf("config_opts['plugin_conf']['ccache_enable'] = False")
-    print_conf(
-        "config_opts['plugin_conf']['root_cache_opts']['compress_program'] = ''")
-    print_conf(
-        "config_opts['plugin_conf']['root_cache_opts']['extension'] = ''")
+    print_conf("config_opts['plugin_conf']['root_cache_opts']['compress_program'] = ''")
+    print_conf("config_opts['plugin_conf']['root_cache_opts']['extension'] = ''")
     print_conf("config_opts['plugin_conf']['root_cache_enable'] = True")
-    print_conf(
-        "config_opts['plugin_conf']['root_cache_opts']['age_check'] = True")
-    print_conf(
-        "config_opts['plugin_conf']['root_cache_opts']['max_age_days'] = 1")
+    print_conf("config_opts['plugin_conf']['root_cache_opts']['age_check'] = True")
+    print_conf("config_opts['plugin_conf']['root_cache_opts']['max_age_days'] = 1")
     print_conf("config_opts['plugin_conf']['package_state_enable'] = True")
     print_conf("config_opts['plugin_conf']['package_state_opts'] = {}")
-    print_conf(
-        "config_opts['plugin_conf']['package_state_opts']['available_pkgs'] = False")
-    print_conf(
-        "config_opts['plugin_conf']['package_state_opts']['installed_pkgs'] = True")
+    print_conf("config_opts['plugin_conf']['package_state_opts']['available_pkgs'] = False")
+    print_conf("config_opts['plugin_conf']['package_state_opts']['installed_pkgs'] = True")
     print_conf(common_string)
     repo_names = repo_names.split()
     repo_urls = repo_url.split()
     repo_conf = dict(zip(repo_names, repo_urls))
-    print_conf("\n".join("[{}]\nname={}\nbaseurl={}{}\ngpgcheck=0\nenabled=1\n".format(
-        k, k, k[:0], v) for k, v in repo_conf.items()))
+    print_conf("\n".join("[{}]\nname={}\nbaseurl={}{}\ngpgcheck=0\nenabled=1\n".format(k, k, k[:0], v) for k, v in repo_conf.items()))
     print_conf('"""')
     # it's a hack to modify time of /etc/mock/default.cfg
     # to prevent root cache recreation
