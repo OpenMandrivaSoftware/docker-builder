2137de94880a6d3e50a41916def97180e8c0f710
alexander stefanov <a.stefanov@yadro.com>
2024-05-13 15:30:31 +0300

    wipe src.rpm in main

--- build-rpm.py
+++ build-rpm.py
@@ -479,10 +479,6 @@ def build_rpm():
                 rpm_packages.append(output_dir + '/' + rpm_pkg)
     # rpm packages
     print(rpm_packages)
-    # remove src.rpm for e2k
-    # MCST request
-    if platform_arch in ['e2kv4', 'e2kv5', 'e2kv6']:
-        remove_if_exist(src_rpm[0])
 
     container_data()
     save_build_root()
@@ -545,3 +541,7 @@ if __name__ == '__main__':
         validate_spec(build_package)
         download_yml(build_package + '/' + '.abf.yml')
         build_rpm()
+        # remove src.rpm for e2k
+        # MCST request
+        if platform_arch in ['e2kv4', 'e2kv5', 'e2kv6']:
+            remove_if_exist(src_rpm[0])
